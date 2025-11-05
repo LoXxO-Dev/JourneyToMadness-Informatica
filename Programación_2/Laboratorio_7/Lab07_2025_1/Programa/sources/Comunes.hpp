42880db88ac141250db5319f5295be1c7941a615
Enzo Fabio Mamani Mamani <a20216421@pucp.edu.pe>
2025-11-05 11:32:32 -0500

    _

--- Programación_2/Laboratorio_7/Lab07_2025_1/Programa/sources/Comunes.hpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by User on 4/11/2025.
-//
-
-#ifndef COMUNES_HPP
-#define COMUNES_HPP
-#include "utils.hpp"
-void imprimirLinea(ofstream &arch,int cant,char car) ;
-#endif //COMUNES_HPP
